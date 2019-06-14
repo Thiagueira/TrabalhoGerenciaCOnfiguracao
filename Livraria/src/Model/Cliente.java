c8641f4e32d5177d2602913cf0086369bd03ece5
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2019-06-14 18:33:22 -0300

    Metodo construtor vazio, get e set

--- Livraria/src/Model/Cliente.java
+++ Livraria/src/Model/Cliente.java
@@ -25,6 +25,59 @@ public class Cliente {
         this.estadoCivil = estadoCivil;
         this.endereco = endereco;
     }
+
+    public Cliente() {
+        endereco = new Endereco();
+    }
+
+    
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
     
     
 }
